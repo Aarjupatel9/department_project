1c874ab89631005c856df3025b3d32b5f8cf1529
Aarju Patel <aarjupatel922003@gmail.com>
2023-09-25 16:46:12 +0530

    after publication module add

--- frontend/src/validator/publicationValidator.jsx
+++ frontend/src/validator/publicationValidator.jsx
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
 import joi from "joi";
-=======
-const joi = require("joi");
-// const { PUBLICATION_TYPE } = require("../utils/constants");
->>>>>>> ece1c6f7d3d0f1fcfe82949e1717bf187cd9c0a9
 const PUBLICATION_TYPE = {
     JOURNALS: "journals",
     CONFERENCE: "conference",
 }
-<<<<<<< HEAD
 
 export const publicationValidator = joi.object({
     userId:joi.string().required(),
-=======
-export const publicationValidator = joi.object({
->>>>>>> ece1c6f7d3d0f1fcfe82949e1717bf187cd9c0a9
     title: joi.string().min(3).max(30).required(),
     description: joi.string().min(10).max(300).required(),
     authors: joi.array().items(joi.string().min(3).max(60)),
@@ -63,11 +54,7 @@ export const publicationValidator = joi.object({
             city: joi.string().required(),
             state: joi.string().required(),
             country: joi.string().required(),
-<<<<<<< HEAD
             zip: joi.string().length(6).required()
-=======
-            zip: joi.string().required()
->>>>>>> ece1c6f7d3d0f1fcfe82949e1717bf187cd9c0a9
         })
     })
 });
